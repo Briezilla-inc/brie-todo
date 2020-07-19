295a023de000d9856b3fdf2b2aa2fbfbd92dde6d
Brie <briecardo@icloud.com>
2020-07-19 12:35:46 -0700

    Updated Git Pull

--- server.js
+++ server.js
@@ -4,16 +4,22 @@ const express = require('express');
 const dotenv  = require('dotenv')
 const colors = require('colors')
 const morgan = require('morgan')
-//This line points dotenv to our config file so it can have access to the stuff in there
+// This line points dotenv to our config file so it can have access to the stuff in there
 dotenv.config({path: './config/config.env'})
 const todos = require('./routes/todos')
 
-//Here we iniatalize express and save it to a variable named 'app'
+// Here we iniatalize express and save it to a variable named 'app'
 const app = express();
 
 // Connect to the todos route 
 app.use('/api/v1/todos', todos)
 
+// How to access the global variables
 const PORT = process.env.PORT || 5000;
-// connect to port 
-app.listen(PORT, console.log(`We connected on ${PORT}!`.yellow.bold))
+
+// Connect to port 
+app.listen(PORT, console.log(`We connected on ${PORT}!`.yellow.bold));
+
+
+
+
