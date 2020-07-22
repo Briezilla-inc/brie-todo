ad500edaa5a149da006461f60e89a904798946ce
Chad Hinds <chadrickj8@gmail.com>
2020-07-22 18:07:00 -0400

    Add new components and axios

---
+++ frontend/src/Components/TodoList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import TodoItem from './TodoItem';
+import TodoItem from './TodoItem';
+import axios from 'axios';
+
+class TodoList extends Component {
+    render() {
+        return (
+            <ul>
+                <TodoItem />
+            </ul>
+        );
+    }
+}
+
+export default TodoList;
