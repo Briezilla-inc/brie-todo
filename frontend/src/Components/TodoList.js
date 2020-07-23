988b55d92864fdcfb277949fdb0cc23eaeb41ef1
Chad Hinds <chadrickj8@gmail.com>
2020-07-23 14:17:10 -0400

    Explain what this component is doing

--- frontend/src/Components/TodoList.js
+++ frontend/src/Components/TodoList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import TodoItem from './TodoItem';
 import axios from 'axios';
 
+// Holds the list on todos 
 class TodoList extends Component {
     render() {
         return (
