1cdae63ea76a1b5e839478b1712d0397d4318a9e
Chad Hinds <chadrickj8@gmail.com>
2020-08-09 18:20:49 -0400

    test if app has buttons

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import todoItem from './Components/TodoItem';
 
 import App from './App';
-
+// After you have rendered your React component(s), React Testing Library offers you different search functions to grab elements
+// Use these elements in your test assertions
 describe('App', () => {
-  test('renders App component', () => {
+  test('renders App component that has buttons', () => {
     render(<App />);
+
+    expect(screen.getByRole('button')).toBeInTheDocument();
   });
 });
