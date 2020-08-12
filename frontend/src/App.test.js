9d83aa493dc68ab1c2a8b86e1b99966e5adebc41
Chad Hinds <chadrickj8@gmail.com>
2020-08-12 11:30:57 -0400

    add test should have placeholder text

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import todoItem from './Components/TodoItem';
+import addTodo from './Components/AddTodo';
 
 import App from './App';
 // After you have rendered your React component(s), React Testing Library offers you different search functions to grab elements
@@ -8,7 +8,11 @@ import App from './App';
 describe('App', () => {
   test('renders App component that has some buttons', () => {
     render(<App />);
-
     expect(screen.getByRole('button')).toBeInTheDocument();
   });
+
+  test('should have placeholder', () => {
+    render(<addTodo placeholder="Add a todo..." />)
+    expect(screen.getByPlaceholderText("Add a todo...")).toBeInTheDocument();
+  })
 });
